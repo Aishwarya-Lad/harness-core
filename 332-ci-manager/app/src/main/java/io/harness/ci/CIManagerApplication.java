673da006a109ff0cb2d8e96e8644d9089c6d66cf
Aishwarya Lad <67022814+Aishwarya-Lad@users.noreply.github.com>
2024-01-08 13:09:17 -0800

    create a base branch from develop

--- 332-ci-manager/app/src/main/java/io/harness/ci/CIManagerApplication.java
+++ 332-ci-manager/app/src/main/java/io/harness/ci/CIManagerApplication.java
@@ -3,7 +3,6 @@
  * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
  * that can be found in the licenses directory at the root of this repository, also available at
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
- * Adding a comment to trigger an initial call graph generation for branch - CI-10058-initial-cg-pr
  */
 
 package io.harness.app;
